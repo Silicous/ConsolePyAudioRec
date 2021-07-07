5ba66147c688b8bd3d7f535ef6932fc6f1382123
Serhio <56162821+Silicous@users.noreply.github.com>
2021-07-07 11:26:48 +0300

    fix argument parameters

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from recorder import Recorder
+import sys
 
 if __name__ == '__main__':
 
-    recorder = Recorder()
+    recorder = Recorder(*sys.argv)
 
     # recorder.show_active_devices()
     # recorder.set_default_device('pulse')
